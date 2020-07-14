8d12745b1d63109a30c66db787ec6576067fd8b6
Amirsalar Heydari <amirsalaar@gmail.com>
2020-07-13 23:24:06 -0700

    Change knexfile

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,5 @@
 import Knex from 'knex';
-import * as knexConfig from './knexfile';
+import knexConfig from './knexfile';
 
 const knexConnector: Knex = Knex(knexConfig);
 
--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,49 +1,19 @@
 import dotenv from 'dotenv';
+import Knex from 'knex';
 
 dotenv.config();
 
-module.exports = {
-	development: {
-		client: 'pg',
-		connection: {
-			database: process.env.POSTGRES_DB,
-			user: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-		},
-		migrations: {
-			directory: './db/migrations',
-		},
+const knexConfig: Knex.Config = {
+	client: 'pg',
+	connection: {
+		database: process.env.POSTGRES_DB,
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
 	},
-
-	staging: {
-		client: 'pg',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password',
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			tableName: 'knex_migrations',
-		},
-	},
-
-	production: {
-		client: 'pg',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password',
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			tableName: 'knex_migrations',
-		},
+	migrations: {
+		directory: './migrations',
+		extension: 'ts',
 	},
 };
+
+export default knexConfig;
